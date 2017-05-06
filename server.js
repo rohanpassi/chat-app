a6eb4433652750ad3b159708ceab6c2bef3b0aa5
Rohan Passi <rohanpassi007@gmail.com>
2017-05-06 14:39:59 +0530

    bug fix

--- server.js
+++ server.js
@@ -43,109 +43,108 @@ app.use(express.static(path.join(__dirname, "public")));
 
 io.on('connection', function(socket) {
   console.log('new connection made');
+});
 
-  // Show all users
-  socket.on('get-all-users', function(){
-    console.log("All Users: ", usersAll);
-    socket.emit('all-users', usersAll);
-  });
+// Show all users
+socket.on('get-all-users', function(){
+  console.log("All Users: ", usersAll);
+  socket.emit('all-users', usersAll);
+});
 
-  socket.on('get-users-room1', function(){
-    console.log("Users in room 1: ", usersRoom1);
-    socket.emit('users-room1', usersRoom1);
-  });
+socket.on('get-users-room1', function(){
+  console.log("Users in room 1: ", usersRoom1);
+  socket.emit('users-room1', usersRoom1);
+});
 
-  socket.on('get-users-room2', function(){
-    console.log("Users in room 2: ", usersRoom2);
-    socket.emit('users-room2', usersRoom2);
-  });
+socket.on('get-users-room2', function(){
+  console.log("Users in room 2: ", usersRoom2);
+  socket.emit('users-room2', usersRoom2);
+});
 
-  socket.on('get-room1-messages', function(){
-    console.log("Messages in room 1: ", room1Messages);
-    socket.emit('room1-messages', room1Messages);
-  });
+socket.on('get-room1-messages', function(){
+  console.log("Messages in room 1: ", room1Messages);
+  socket.emit('room1-messages', room1Messages);
+});
 
-  socket.on('get-room2-messages', function(){
-    console.log("Messages in room 1: ", room2Messages);
-    socket.emit('room2-messages', room2Messages);
-  });
+socket.on('get-room2-messages', function(){
+  console.log("Messages in room 1: ", room2Messages);
+  socket.emit('room2-messages', room2Messages);
+});
 
-  // When new socket joins in room 1
-  socket.on('join-room1', function(data){
-  	socket.nickname = data.nickname;
-  	usersRoom1[socket.nickname] = socket;
-  	var userObj = {
-  		nickname: data.nickname,
-  		socketid: socket.id
-  	}
-  	usersRoom1.push(userObj);
-    flag = false;
+// When new socket joins in room 1
+socket.on('join-room1', function(data){
+  socket.nickname = data.nickname;
+  usersRoom1[socket.nickname] = socket;
+  var userObj = {
+    nickname: data.nickname,
+    socketid: socket.id
+  }
+  usersRoom1.push(userObj);
+  flag = false;
 
-    for(var user of usersAll){
-      if(user.nickname == userObj.nickname){
-        flag = true;
-        break;
-      }
+  for(var user of usersAll){
+    if(user.nickname == userObj.nickname){
+      flag = true;
+      break;
     }
-    if(!flag){
-      usersAll.push(userObj);
+  }
+  if(!flag){
+    usersAll.push(userObj);
+  }
+  io.emit('users-room1', usersRoom1);
+});
+
+// Whem new socket joins in room 2
+socket.on('join-room2', function(data){
+  socket.nickname = data.nickname;
+  usersRoom2[socket.nickname] = socket;
+  var userObj = {
+    nickname: data.nickname,
+    socketid: socket.id
+  }
+  usersRoom2.push(userObj);
+  flag = false;
+  for(var user of usersAll){
+    if(user.nickname == userObj.nickname){
+      flag = true;
+      break;
     }
-  	io.emit('users-room1', usersRoom1);
+  }
+  if(!flag){
+    usersAll.push(userObj);
+  }
+  io.emit('users-room2', usersRoom2);
+});
+
+// Broadcast the message in room1 users
+socket.on('send-message-room1', function(data){
+  room1Messages.push(data);
+  socket.broadcast.emit('message-received-room1', data);
+});
+
+// Broadcast the message in room 2 users
+socket.on('send-message-room2', function(data){
+  room2Messages.push(data);
+  socket.broadcast.emit('message-received-room2', data);
+});
+
+// Disconnect from socket
+socket.on('disconnect', function(){
+  usersAll = usersAll.filter(function(item){
+    console.log(item.nickname + " disconnected")
+    return item.nickname !== socket.nickname;
   });
 
-  // Whem new socket joins in room 2
-  socket.on('join-room2', function(data){
-    socket.nickname = data.nickname;
-    usersRoom2[socket.nickname] = socket;
-    var userObj = {
-      nickname: data.nickname,
-      socketid: socket.id
-    }
-    usersRoom2.push(userObj);
-    flag = false;
-    for(var user of usersAll){
-      if(user.nickname == userObj.nickname){
-        flag = true;
-        break;
-      }
-    }
-    if(!flag){
-      usersAll.push(userObj);
-    }
-    io.emit('users-room2', usersRoom2);
+  usersRoom1 = usersRoom1.filter(function(item){
+    return item.nickname !== socket.nickname;
   });
 
-  // Broadcast the message in room1 users
-  socket.on('send-message-room1', function(data){
-    room1Messages.push(data);
-    socket.broadcast.emit('message-received-room1', data);
+  usersRoom2 = usersRoom2.filter(function(item){
+    return item.nickname !== socket.nickname;
   });
 
-  // Broadcast the message in room 2 users
-  socket.on('send-message-room2', function(data){
-    room2Messages.push(data);
-    socket.broadcast.emit('message-received-room2', data);
-  });
-
-  // Disconnect from socket
-  socket.on('disconnect', function(){
-    usersAll = usersAll.filter(function(item){
-      console.log(item.nickname + " disconnected")
-      return item.nickname !== socket.nickname;
-    });
-
-    usersRoom1 = usersRoom1.filter(function(item){
-      return item.nickname !== socket.nickname;
-    });
-
-    usersRoom2 = usersRoom2.filter(function(item){
-      return item.nickname !== socket.nickname;
-    });
-
-    io.emit('users-room1', usersRoom1);
-    io.emit('users-room2', usersRoom2);
-  });
-
+  io.emit('users-room1', usersRoom1);
+  io.emit('users-room2', usersRoom2);
 });
 
 server.listen(port, function() {
